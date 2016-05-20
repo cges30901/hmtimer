a27a30e5b5e43273edbaa5dfc92e2ea387233e94
Hsiu-Ming Chang <cges30901@gmail.com>
2016-05-20 16:11:10 +0800

    version 2.3

--- hmtimer/mainwindow.cpp
+++ hmtimer/mainwindow.cpp
@@ -301,7 +301,7 @@ void MainWindow::on_actionAbout_Qt_triggered()
 
 void MainWindow::on_actionAbout_triggered()
 {
-    QString version="2.2";
+    QString version="2.3";
     QMessageBox::about(this,tr("About"),tr("<h3>Hsiu-Ming's Timer %1</h3><br>"
                                            "Author: Hsiu-Ming Chang<br>"
                                            "e-mail: cges30901@gmail.com<br>"
