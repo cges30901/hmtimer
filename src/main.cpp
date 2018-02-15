d45349e661e6041c8f8894bdd18b2c5b501d49f9
Hsiu-Ming Chang <cges30901@gmail.com>
2018-02-15 13:25:44 +0800

    add --help and --version in help and man

--- src/main.cpp
+++ src/main.cpp
@@ -29,12 +29,14 @@ int main(int argc,char *argv[])
     bool showWindow=true;
     for(int i=0;i<args.size();i++){
         if(args.at(i)=="--help"){
-            std::cout<<"Usage: hmtimer [-t time] [-s] [-m] [-a ACTION]\n"
+            std::cout<<"Usage: hmtimer [-t time] [-s] [-m] [-a ACTION] [--help] [--version]\n"
                      <<"options:\n"
                      <<"  -t SECONDS   set seconds and start timer\n"
                      <<"  -m           minimize to tray\n"
                      <<"  -a ACTION    set action when time is up.\n"
-                     <<"    available action: monitor, shutdown, reboot, sound, runprogram\n";
+                     <<"    available action: monitor, shutdown, reboot, sound, runprogram\n"
+                     <<"  --help       display help and exit\n"
+                     <<"  --version    display version and exit\n";
             exit(0);
         }
         else if(args.at(i)=="--version"){
